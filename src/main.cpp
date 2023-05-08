9dc9ffe606ae5167e7b6207350001a63e7774f9e
Vassterak <vaclavhorcicka@outlook.cz>
2023-05-08 21:41:06 +0200

    Simple initial test

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include <Arduino.h>
 
+#define LEDPIN GPIO_NUM_23
+
 void setup()
 {
   // put your setup code here, to run once:
+  pinMode(LEDPIN, OUTPUT);
+
 }
 
 void loop()
 {
+  digitalWrite(LEDPIN, HIGH);
+  delay(1000);
+  digitalWrite(LEDPIN, LOW);
+  delay(1000);
   // put your main code here, to run repeatedly:
 }
